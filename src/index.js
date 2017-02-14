4ce2722d6d576450f4795b55a0efacc7e20d649e
James Greenaway <james@100shapes.com>
2017-02-14 15:57:55 +0000

    Add tweening cube rotation controls

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {
 import { OrbitModule } from '@whs:controls/orbit'
 
 import { App } from '@whs/core/App'
+import { Loop } from '@whs/core/Loop'
 
 import { AmbientLight } from '@whs+lights/AmbientLight'
 import { PointLight } from '@whs+lights/PointLight'
@@ -31,7 +32,45 @@ import {
   ConvexModule,
 } from 'physics-module-ammonext'
 
+import TWEEN from 'tween.js'
+import snapLerp from 'snap-lerp'
+
+
 const noop = () => {}
+
+const boxWall = (attrs = {}, size = 100) => new Box({
+  ...attrs,
+
+  geometry: {
+    width: size,
+    height: size,
+    depth: 0,
+  },
+
+  shadow: {
+    cast: false,
+    receive: false,
+  },
+
+  modules: [
+    new BoxModule({
+      mass: 100,
+    })
+  ],
+
+  material: boxMaterial,
+})
+
+
+const ballMaterial = new MeshNormalMaterial({
+  shading: FlatShading,
+})
+
+const boxMaterial = new MeshNormalMaterial({
+  shading: FlatShading,
+  transparent: true,
+  opacity: 0.5,
+})
 
 
 export default function init(
@@ -43,18 +82,6 @@ export default function init(
     world.setGravity(new Vector3(...args))
     onGravity(args)
   }
-
-
-  const ballMaterial = new MeshNormalMaterial({
-    shading: FlatShading,
-  })
-
-  const boxMaterial = new MeshNormalMaterial({
-    shading: FlatShading,
-    transparent: true,
-    opacity: 0.5,
-  })
-
 
   const mouse = new VirtualMouseModule()
 
@@ -85,32 +112,48 @@ export default function init(
     mouse,
   ])
 
+  // Ball
+  new Icosahedron({
+    geometry: {
+      radius: 30,
+      detail: 2,
+    },
 
-  // Create all sides of the box
-  function boxWall(attrs = {}, size = 100) {
-    return new Box({
-      ...attrs,
+    modules: [
+      new ConvexModule({
+        mass: 1,
+        restitution: 3.5,
+        friction: 0,
+      }),
+    ],
 
-      geometry: {
-        width: size,
-        height: size,
-        depth: 0,
-      },
+    material: ballMaterial,
 
-      shadow: {
-        cast: false,
-        receive: false,
-      },
+    position: [0, 20, 0],
+  }).addTo(world)
 
-      modules: [
-        new BoxModule({
-          mass: 100,
-        })
-      ],
+  // Light
+  new PointLight({
+    light: {
+      intensity: 1,
+      distance: 1000,
+    },
 
-      material: boxMaterial,
-    })
-  }
+    shadow: {
+      fov: 500,
+    },
+
+    position: [10, 10, 100],
+  })
+  .addTo(world)
+
+  // Light
+  new AmbientLight({
+    light: {
+      intensity: 0.5,
+    },
+  }).addTo(world)
+
 
   // Create wireframe box
   const box = new Box({
@@ -201,73 +244,72 @@ export default function init(
       // Click box to change state
       box.on('click', cycleStates)
 
-      function setRotation(...args) {
-        
+      const t = 0.1
+      const min = 0.0001
+
+      const rotationFactor = 1.5
+      const targetRotation = { x: box.rotation.x, y: box.rotation.y, z: box.rotation.z }
+
+      const setRotationGeometry = (...args) => {
         // Geometric rotation
-        // box.rotation.x = args[0]
-        // box.rotation.y = args[1]
-        // box.rotation.z = args[2]
-        // box.__dirtyRotation = true
+        targetRotation.x = args[0] * rotationFactor
+        targetRotation.y = args[1] * rotationFactor
+        targetRotation.z = args[2] * rotationFactor
+        
+        // Callback
+        onRotation(args)
+      }
 
+
+      const velocityFactor = 4
+      const targetVelocity = { x: 0, y: 0, z: 0 }
+
+      const setRotationVelocity = (...args) => {
         // Velocity change
-        const factor = 5
-        const v = new Vector3(args[0] * factor, args[1] * factor, args[2] * factor)
-        box.setAngularVelocity(v)
-
+        targetVelocity.x = args[0] * velocityFactor
+        targetVelocity.y = args[1] * velocityFactor
+        targetVelocity.z = args[2] * velocityFactor
+        
+        // Callback
         onRotation(args)
       }
 
       // Move box with mouse
       mouse.on('move', () => {
-        setRotation(-mouse.y, mouse.x, 0)
+        // setRotationGeometry(-mouse.y, mouse.x, 0)
+        setRotationVelocity(-mouse.y, mouse.x, 0)
       })
+
+      // Update cube rotation geometry
+      // new Loop(() => {
+      //   box.rotation.x = snapLerp(box.rotation.x, targetRotation.x, t)
+      //   box.rotation.y = snapLerp(box.rotation.y, targetRotation.y, t)
+      //   box.rotation.z = snapLerp(box.rotation.z, targetRotation.z, t)
+      //   box.__dirtyRotation = true
+      // }).start(world)
+
+      // Update cube roation velocity
+      new Loop(() => {
+        box.setAngularVelocity(new Vector3(targetVelocity.x, targetVelocity.y, targetVelocity.z))
+      }).start(world)
+
+
+      // new Loop((clock) => {
+      //   TWEEN.update(clock.getElapsedTime())
+      // }).start(world)
+      
+      // setTimeout(() => {
+      //   function animate(time) {
+      //     requestAnimationFrame(animate)
+      //     TWEEN.update(time)
+      //   }
+      //   animate()
+      //   tweenCube.start()
+      // }, 1000)
+
+      world.start()
     })
   })
-
-
-  // Create a ball
-  new Icosahedron({
-    geometry: {
-      radius: 30,
-      detail: 2,
-    },
-
-    modules: [
-      new ConvexModule({
-        mass: 10,
-        restitution: 3,
-        friction: 1,
-      }),
-    ],
-
-    material: ballMaterial,
-
-    position: [0, 30, 0],
-  }).defer(ball => {
-    ball.addTo(world)
-  })
-
-  new PointLight({
-    light: {
-      intensity: 1,
-      distance: 1000,
-    },
-
-    shadow: {
-      fov: 500,
-    },
-
-    position: [10, 10, 100],
-  })
-  .addTo(world)
-
-  new AmbientLight({
-    light: {
-      intensity: 0.5,
-    },
-  }).addTo(world)
-
-  world.start()
 
   return function destory() {
     console.log('TODO: Destory spatial cube')
